f3169360abfae3568c2a8c8581d35553d37b9e92
Rahul Singh <rahulrsk07@gmail.com>
2020-01-09 21:10:19 +0530

    File to include all the project related constants

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+#   Primary Author: Rahul Singh <rahulrsk07@gmail.com>
+#
+#   Purpose: This file contains the constant values used in our project
+
+"""
+Entities
+"""
+CATEGORY = "category"
+SUB_CATEGORY = "sub_category"
+ITEM = "item"

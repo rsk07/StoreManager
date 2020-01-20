bd2aac1a901fe9beb61cd8cdc137171439dbf4cc
Rahul Singh <rahulrsk07@gmail.com>
2020-01-20 21:10:19 +0530

    Added units standard mapping

--- src/constants.py
+++ src/constants.py
@@ -8,3 +8,17 @@ Entities
 CATEGORY = "category"
 SUB_CATEGORY = "sub_category"
 ITEM = "item"
+
+"""
+Unit conversion mapping
+"""
+units_mapping = {
+    'gm': {
+        'std_equivalent_val': 0.001,
+        'std_equivalent_unit': 'kg'
+    },
+    'ml': {
+        'std_equivalent_val': 0.001,
+        'std_equivalent_unit': 'lt'
+    }
+}

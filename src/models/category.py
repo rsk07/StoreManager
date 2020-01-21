7c28829c83fba1cd8659a1428ffb6c991d521f11
Rahul Singh <rahulrsk07@gmail.com>
2020-01-21 21:10:19 +0530

    Added function to validate arguments

--- src/models/category.py
+++ src/models/category.py
@@ -2,6 +2,38 @@
 #
 #   Purpose: This file contains entity class for category
 
+from typing import Any
 
-class Category:
-    pass
+from src.models.entity import Entity
+
+
+class Category(Entity):
+    """
+    This is the category entity class.
+    """
+
+    def __init__(self, name: str, discount_str: str) -> None:
+        """
+        Initialization method for category entity class.
+
+        Args:
+            name: category's name
+            discount_str: category's discount string
+        """
+
+        self.name = name
+        self.discount_strategy = Entity.factory_for_discount(discount_str)(discount_str)
+
+    @staticmethod
+    def validate_args(*args: Any) -> bool:
+        """
+        Validates args for category entity.
+
+        Args:
+            *args: args to be validated
+
+        Returns:
+            True, if valid, else False
+        """
+
+        return len(args) == 2 and Entity.validate_discount(args[1])

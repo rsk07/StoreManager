55d9d13da3d1a1716558193ac2349096fc10baaa
Rahul Singh <rahulrsk07@gmail.com>
2020-01-28 21:10:19 +0530

    Added function to validate price

--- src/models/category.py
+++ src/models/category.py
@@ -37,3 +37,33 @@ class Category(Entity):
         """
 
         return len(args) == 2 and Entity.validate_discount(args[1])
+
+    @staticmethod
+    def validate_price(price_str: str) -> bool:
+        """
+        Validates the price string for item.
+
+        Args:
+            price_str: string to be validated
+
+        Returns:
+            True, if valid, else False
+        """
+
+        # if no digit is found, return false
+        if not extract_required_data(data_str=price_str, req_type=r'\d+'):
+            return False
+
+        # if per('/') is not found, return false
+        if '/' not in price_str:
+            print("Please specify item price per ('/') units")
+            return False
+
+        # extract the unit from the price string
+        unit = price_str[price_str.index('/') + 1:]
+
+        # is unit not found in stored units, return false
+        if not StandardUnits.has_value(unit) and unit not in units_mapping:
+            return False
+
+        return True

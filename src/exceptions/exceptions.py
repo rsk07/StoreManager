d12ffda1654f5165bde5f28431ec44ca392acc43
Rahul Singh <rahulrsk07@gmail.com>
2020-01-29 21:10:19 +0530

    Added read file exception

---
+++ src/exceptions/exceptions.py
@@ -0,0 +1,7 @@
+#   Primary Author: Rahul Singh <rahulrsk07@gmail.com>
+#
+#   Purpose: This file containing exceptions
+
+
+class ReadFileError(Exception):
+    """ Raise this when input file could not be read. """

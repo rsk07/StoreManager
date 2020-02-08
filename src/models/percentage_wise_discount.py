130afa0b95264e28c2eeb05ea2a44cab08a3a444
Rahul Singh <rahulrsk07@gmail.com>
2020-02-08 23:10:19 +0530

    Added class for percentage wise discount strategy

---
+++ src/models/percentage_wise_discount.py
@@ -0,0 +1,9 @@
+#   Primary Author: Rahul Singh <rahulrsk07@gmail.com>
+#
+#   Purpose: This file contains the percentage wise discount class
+
+from src.models.discount import DiscountStrategy
+
+
+class PercentageWiseDiscountStrategy(DiscountStrategy):
+    pass

18336136333999c166303611c9fc03d1bb9702bf
Rahul Singh <rahulrsk07@gmail.com>
2020-02-12 23:10:19 +0530

    Added function to find discount unit wise

--- src/models/percentage_wise_discount.py
+++ src/models/percentage_wise_discount.py
@@ -6,4 +6,32 @@ from src.models.discount import DiscountStrategy
 
 
 class PercentageWiseDiscountStrategy(DiscountStrategy):
-    pass
+    """
+    This is the percentage wise discount strategy class
+    """
+
+    def __init__(self, discount_str: str) -> None:
+        """
+        Initialization method for percentage wise discount class.
+
+        Args:
+            discount_str: discount string containing the discount in percentage
+        """
+
+        self.discount = int(discount_str.strip('%'))
+
+    @staticmethod
+    def validate(discount_str: str) -> bool:
+        """
+        Validates the discount string.
+
+        Args:
+            discount_str:
+
+        Returns:
+            True if valid, else False
+        """
+
+        discount_temp = discount_str.strip('%')
+
+        return discount_temp.isdigit() and (0 <= int(discount_temp) <= 100)

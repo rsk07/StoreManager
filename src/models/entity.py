9ea9edc19b74e3131a37d3b87ae2b087e21b9265
Rahul Singh <rahulrsk07@gmail.com>
2020-02-25 23:10:19 +0530

    Added function to fetch max discount

--- src/models/entity.py
+++ src/models/entity.py
@@ -5,6 +5,11 @@
 from typing import Any
 from abc import abstractmethod
 
+from src.exceptions.exceptions import InvalidDiscountString
+from src.models.discount import DiscountStrategy
+from src.models.item_wise_discount import ItemWiseDiscountStrategy
+from src.models.percentage_wise_discount import PercentageWiseDiscountStrategy
+
 
 class Entity:
     """
@@ -21,3 +26,25 @@ class Entity:
         """
 
         pass
+
+    @staticmethod
+    def factory_for_discount(discount_str: str) -> DiscountStrategy():
+        """
+        This is a factory class for discount. This is will let us know what kind of discount has been provided.
+
+        Args:
+            discount_str: str containing discount data
+
+        Returns:
+            corresponding discount class
+        """
+
+        # if percentage is found in the str, use percentage discount strategy, else use item wise discount strategy
+        if '%' in discount_str:
+            return PercentageWiseDiscountStrategy
+
+        if '+' in discount_str:
+            return ItemWiseDiscountStrategy
+
+        # raise exception if discount string doesn't match any format
+        raise InvalidDiscountString
